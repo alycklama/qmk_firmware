067681f3ebc89b0e4b69d3d8bad47cb667ea3e95
alycklama <>
2024-11-23 12:25:59 +0100

    Ports my keymap to firmware24

---
+++ keyboards/zsa/voyager/keymaps/alycklama/config.h
@@ -0,0 +1,33 @@
+/*
+  Set any config.h overrides for your specific keymap here.
+  See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
+*/
+
+#define PERMISSIVE_HOLD
+
+#define USB_SUSPEND_WAKEUP_DELAY 0
+#undef MOUSEKEY_WHEEL_DELAY
+#define MOUSEKEY_WHEEL_DELAY 100
+
+#undef MOUSEKEY_MAX_SPEED
+#define MOUSEKEY_MAX_SPEED 5
+
+#undef MOUSEKEY_TIME_TO_MAX
+#define MOUSEKEY_TIME_TO_MAX 30
+
+#undef MOUSEKEY_WHEEL_MAX_SPEED
+#define MOUSEKEY_WHEEL_MAX_SPEED 5
+
+#undef MOUSEKEY_WHEEL_TIME_TO_MAX
+#define MOUSEKEY_WHEEL_TIME_TO_MAX 20
+
+#define SERIAL_NUMBER "6RaDq/Vl0VM"
+#define LAYER_STATE_8BIT
+#define COMBO_COUNT 1
+
+#define RGB_MATRIX_STARTUP_SPD 30
+#define RGB_MATRIX_TIMEOUT 300000 // number of milliseconds to wait until RGB automatically turns off
+
+#define LEADER_PER_KEY_TIMING
+#define LEADER_TIMEOUT 250
+
