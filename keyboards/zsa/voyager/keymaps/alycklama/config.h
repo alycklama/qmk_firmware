5d1746a04e73152a863a94a35b2b076a788ba8ce
alycklama <>
2025-03-21 22:41:31 +0200

    Sets TAPPING_TERM to 300 milliseconds

--- keyboards/zsa/voyager/keymaps/alycklama/config.h
+++ keyboards/zsa/voyager/keymaps/alycklama/config.h
@@ -3,6 +3,9 @@
   See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
 */
 
+#undef TAPPING_TERM
+#define TAPPING_TERM 300
+
 #define PERMISSIVE_HOLD
 
 #define USB_SUSPEND_WAKEUP_DELAY 0
