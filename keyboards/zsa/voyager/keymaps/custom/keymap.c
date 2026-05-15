4c2c09a4530526af82dfe6c0b57a136a5c4498d2
Anton Lijcklama à Nijeholt <>
2026-05-15 10:27:18 +0300

    Swaps layers 2 and 4

--- keyboards/zsa/voyager/keymaps/custom/keymap.c
+++ keyboards/zsa/voyager/keymaps/custom/keymap.c
@@ -41,7 +41,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,            KC_Q,  KC_W,               KC_E,               KC_R,               KC_T,                                           KC_Y,           KC_U,        KC_I,                          KC_O,        KC_P,    KC_EQUAL,
     DUAL_FUNC_0,       KC_A,  KC_S,               KC_D,               KC_F,               KC_G,                                           KC_H,           KC_J,        KC_K,                          KC_L,        KC_SCLN, KC_QUOTE,
     KC_LEFT_SHIFT,     KC_Z,  MT(MOD_LCTL, KC_X), MT(MOD_LALT, KC_C), MT(MOD_LGUI, KC_V), KC_B,                                           KC_N,           MT(MOD_RGUI, KC_M), MT(MOD_RALT, KC_COMMA), MT(MOD_RCTL, KC_DOT), KC_SLASH, KC_RIGHT_SHIFT,
-                                                                                          LT(2, KC_ENTER),LT(3, KC_BSPC),      KC_DELETE, LT(4, KC_SPACE)
+                                                                                          LT(4, KC_ENTER),LT(3, KC_BSPC),      KC_DELETE, LT(2, KC_SPACE)
   ),
   // Mouse
   [1] = LAYOUT_voyager(
