0d1f29a5a8a33a7e7e6a027279ffdc2635ba9e34
Anton Lijcklama à Nijeholt <>
2026-05-15 10:26:12 +0300

    Refactors key_overrides

--- keyboards/zsa/voyager/keymaps/custom/keymap.c
+++ keyboards/zsa/voyager/keymaps/custom/keymap.c
@@ -17,11 +17,11 @@ const key_override_t grave_esc_override = ko_make_basic(MOD_MASK_GUI, ALL_T(KC_E
 const key_override_t delete_key_override = ko_make_basic(MOD_MASK_SHIFT, LT(3,KC_BSPC), KC_DEL);
 
 // This globally defines all key overrides to be used
-const key_override_t **key_overrides = (const key_override_t *[]){
-	&tilde_esc_override,
-	&grave_esc_override,
-	&delete_key_override,
-	NULL // Null terminate the array of overrides!
+const key_override_t *key_overrides[] = {
+    &tilde_esc_override,
+    &grave_esc_override,
+    &delete_key_override,
+    NULL // Null terminate the array of overrides!
 };
 
 enum custom_keycodes {
